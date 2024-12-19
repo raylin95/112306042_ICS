6ea984d66952a759058edbf59f0829152c2749e2
raylin95 <147122603+raylin95@users.noreply.github.com>
2024-12-20 00:32:57 +0800

    HW7

--- DataSturcture/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        int numOfDisks = 5; // 設定盤子的數量
-        HanoiTower tower = new HanoiTower(numOfDisks);
-        tower.start();
-        System.out.println("Total moves: " + tower.getTimes());
-    }
-}
